3244e86f58989cb5ed4f6f41993b984d5c01c531
VictorSalome <victor.sousa@medsystems.com.br>
2025-07-06 00:52:03 -0300

    Refactor code structure for improved readability and maintainability

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${process.env.NODE_ENV} mode`);

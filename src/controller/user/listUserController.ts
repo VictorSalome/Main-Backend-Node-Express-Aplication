fd6f0204886bedf69f38a49a57052a7092004371
VictorSalome <victor.sousa@medsystems.com.br>
2025-07-10 02:12:38 -0300

    feat: Implement user listing controller to retrieve and return user data

---
+++ src/controller/user/listUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, RequestHandler } from "express";
+
+import { getListOfUsers } from "../../model/user/registerUserModel";
+
+export const getListUserController: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const users = await getListOfUsers();
+    res.status(200).json({
+      message: "Lista de usuários",
+      data: users,
+    });
+  } catch (error) {
+    console.error("Erro ao listar usuários:", error);
+    res.status(500).json({
+      message: "Erro interno do servidor",
+    });
+  }
+};

b01af1b93c0b4ac8481a71a153673d71cebf2615
VictorSalome <victor.sousa@medsystems.com.br>
2025-07-19 23:59:53 -0300

    feat: Add routes for user activation and deactivation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import { getListUserController } from "../controller/user/listUserController";
 import { postLoginUserController } from "../controller/auth/loginUserController";
 import { updateUserController } from "../controller/user/updateUserController";
 import { authenticateJWT } from "../middlewares/auth/userMiddleware";
+import { deactivateUserController } from "../controller/user/deactivateUserController";
+import { activateUserController } from "../controller/user/activateUserController";
 
 const Routes = Router();
 
@@ -11,6 +13,9 @@ Routes.post("/auth/register", postRegisterUserController);
 Routes.post("/auth/login", postLoginUserController);
 
 Routes.get("/auth/users", getListUserController);
+
 Routes.put("/auth/user", authenticateJWT, updateUserController);
+Routes.put("/auth/user/deactivate", authenticateJWT, deactivateUserController);
+Routes.put("/auth/user/activate", authenticateJWT, activateUserController);
 
 export default Routes;

76d390a983bec1d12732a1b3197d499feae27916
VictorSalome <victor.sousa@medsystems.com.br>
2025-07-20 00:00:07 -0300

    feat: Add registration validation schema for user registration

---
+++ src/validates/registerUserValidade.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+export const registerUserSchema = z
+  .object({
+    nameUser: z
+      .string()
+      .min(3, "Nome deve ter pelo menos 3 caracteres")
+      .max(50, "Nome deve ter no máximo 50 caracteres")
+      .trim()
+      .refine(
+        val => /^[\p{L}\p{M}\p{N}_\s]+$/u.test(val),
+        "Nome só pode conter letras, números, _ e espaços"
+      ),
+
+    email: z.string().email("Email deve ter formato válido").toLowerCase(),
+
+    password: z
+      .string()
+      .min(6, "Senha deve ter pelo menos 6 caracteres")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+        "Senha deve ter pelo menos: 1 letra minúscula, 1 maiúscula e 1 número"
+      ),
+
+    confirmPassword: z.string().min(6, "Confirmação de senha é obrigatória"),
+
+    isActive: z.boolean().default(true),
+  })
+  .refine(
+    data => {
+      return data.password === data.confirmPassword;
+    },
+    {
+      message: "Senhas não coincidem",
+      path: ["confirmPassword"],
+    }
+  );
+
+export type RegisterUserInput = z.infer<typeof registerUserSchema>;
